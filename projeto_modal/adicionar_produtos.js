34aad4c105040bd2acc63a36a499f9011e9dec24
ebuarbin <eduvardo16@gmail.com>
2024-10-31 11:50:11 -0300

    cont

---
+++ projeto_modal/adicionar_produtos.js
@@ -0,0 +1,11 @@
+const modal = document.querySelector("dialog")
+const buttonOpen = document.querySelector(".buttonOpen");
+const buttonClose = document.querySelector(".buttonClose")
+
+buttonOpen.addEventListener("click", function() {
+    modal.showModal();
+});
+
+buttonClose.addEventListener("click", function() {
+    modal.close();
+});
