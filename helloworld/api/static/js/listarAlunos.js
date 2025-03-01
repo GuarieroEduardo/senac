c91d289c047e5a8035e980b9ffbbeb3263fe2098
GuarieroEduardo <eduvardo16@gmail.com>
2025-02-28 21:39:39 -0400

    feito

---
+++ helloworld/api/static/js/listarAlunos.js
@@ -0,0 +1,11 @@
+async function deletarAluno(id){
+    const resposta = await fetch(`/api/alunos/${id}`, {
+        method: 'DELETE'
+    })
+
+    if(resposta.ok){
+        var linhaAluno = document.getElementById(`aluno${id}`);
+        linhaAluno.remove();
+    }
+}
+
--- helloworld/api/urls.py
+++ helloworld/api/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views.api_views import AlunoViewSet
 from .views.web_views import home
+from .views.web_views import CriarAluno
 
 router = DefaultRouter()    
 
@@ -13,6 +14,6 @@ router.register('alunos', AlunoViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),  
     path('home/', home, name='home'),
-      
+    path('criarAluno/', CriarAluno, name='criarAluno'),      
 ]
 
